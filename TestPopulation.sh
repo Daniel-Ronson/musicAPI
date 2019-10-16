8f20bdf1bf03b3531ae283c0e897cf400f9e4f6e
ArthurDayot <ArthurDayot@users.noreply.github.com>
2019-10-15 22:49:18 -0700

    Add files via upload

---
+++ TestPopulation.sh
@@ -0,0 +1,20 @@
+curl \
+  --header "Content-type: application/json" \
+  --request PUT \
+  --data '{"id": 4, "title": "Test Title 4", "artist": "Test note", "duration":"3:40","url":"C://songs/s23","arturl":"C;//song/img/s23"}' \
+  http://localhost:3000
+curl \
+  --header "Content-type: application/json" \
+  --request PUT \
+  --data '{"id": 5, "title": "Test Title 4", "artist": "Test note", "duration":"3:40","url":"C://songs/s23","arturl":"C;//song/img/s23"}' \
+  http://localhost:3000
+curl \
+  --header "Content-type: application/json" \
+  --request PUT \
+  --data '{"id": 6, "title": "Test Title 4", "artist": "Test note", "duration":"3:40","url":"C://songs/s23","arturl":"C;//song/img/s23"}' \
+  http://localhost:3000
+curl \
+  --header "Content-type: application/json" \
+  --request PUT \
+  --data '{"id": 7, "title": "Test Title 4", "artist": "Test note", "duration":"3:40","url":"C://songs/s23","arturl":"C;//song/img/s23"}' \
+  http://localhost:3000
