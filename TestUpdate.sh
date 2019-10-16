5261be633304a66fc71beca6f0666d7bb10b91c0
ArthurDayot <ArthurDayot@users.noreply.github.com>
2019-10-16 15:37:27 -0700

    Added another PUT and modified previous

--- TestUpdate.sh
+++ TestUpdate.sh
@@ -1,10 +1,15 @@
 curl \
   --header "Content-type: application/json" \
   --request PUT \
-  --data '{"changeColumn":"title","changeValueTo":"Test Update 1", "artist": "Test artist 1","title":"Test Insert song 2"}' \
+  --data '{"changeColumn":"title","changeValueTo":"Test Update 1 to first inserted song", "artist": "Test artist 1","title":"Test Insert song 1"}' \
   http://127.0.0.1:5100/api/resources/tracks/update
 curl \
   --header "Content-type: application/json" \
   --request PUT \
-  --data '{"changeColumn":"title","changeValueTo":"Test Update 2","id":"4"}' \
+  --data '{"changeColumn":"title","changeValueTo":"Test Update of title to song id = 4","id":"4"}' \
+  http://127.0.0.1:5100/api/resources/tracks/update
+curl \
+  --header "Content-type: application/json" \
+  --request PUT \
+  --data '{"changeColumn":"artist","changeValueTo":"Test Update of artist to song id = 5","id":"5"}' \
   http://127.0.0.1:5100/api/resources/tracks/update
