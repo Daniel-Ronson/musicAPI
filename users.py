d62e3310df55fcad9fc23c4918da0b8f8f68d744
Jason Mora-Mendoza <jasonmoramendoza@gmail.com>
2019-10-15 21:32:26 -0700

    Added hashed password

--- users.py
+++ users.py
@@ -6,8 +6,13 @@ import flask_api
 from flask import request, jsonify
 from flask_api import status, exceptions
 import pugsql
+
+from werkzeug.security import generate_password_hash,check_password_hash
+
+
 app = flask_api.FlaskAPI(__name__)
 app.config.from_envvar('APP_CONFIG')
+
 queries = pugsql.module('queries/')
 queries.connect(app.config['DATABASE_URL'])
 
@@ -25,6 +30,12 @@ def init_db():
 def home():
     return '''<h1>SPOTIFY, but without music streaming</h1>
 <p>A prototype API for delivering track, playlist, and user data.</p>'''
+
+@app.route('/api/resources/user', methods=['GET'])
+def user():
+    if request.method == 'GET':
+        query_paramenrs = request.args
+
 
 @app.route('/api/resources/users/all', methods=['GET'])
 def all_users():
@@ -50,12 +61,18 @@ def updates():
     if request.method == 'PUT':
         return update_user(request.data)
 
-#When posting to flask api, erase trailing whitespaces,
-#{"title":"Blue Submarine","album":"Yellow Submarine","artist":"The Beatles","duration":"3:20","url":"C://songs/s24","arturl":"C;//song/img/s24"},{"title":"Yellow Submarine","album":"Yellow Submarine","artist":"The Beatles","duration":"3:20","url":"C://songs/s23","arturl":"C;//song/img/s23"}
-#{"title":"Yellow Submarine","album":"Yellow Submarine","artist":"The Beatles","duration":"3:20","url":"C://songs/s23","arturl":"C;//song/img/s23"}
+@app.route('api/resources/user', methods=['POST'])
 def create_user(user):
     user = request.data
     required_fields = ['username', 'password', 'firstname', 'lastname','email']
+    username = data['username']
+    password = data['password']
+    firstname = data['firstname']
+    lastname = data['lastname']
+    email = data['email']
+    hashed_password = generate_password_hash(password)
+    query ="INSERT INTO users(username, hashed_password, firstname, lastname, email) VALUEs('"+username+"','"+hashed_password+"', '"+firstname+"', '"+lastname+"', '"+email+"' );"
+    print(query)
 
     if not all([field in user for field in required_fields]):
         raise exceptions.ParseError()
@@ -66,26 +83,21 @@ def create_user(user):
 
     return user, status.HTTP_201_CREATED
 
-#Requires 'id' or 'artist title'
-#{"changeColumn":"title","changeValueTo":"new song name", "artist": "The beatles","title":"Yellow Submarine",id":"1"}
 def update_user(user):
     search_by_id = ['columnName','columnValue','id']
-    search_by_unique_constraint = ['columnName','columnValue','username']
+    search_by_unique_constraint = ['columnName','columnValue','hashed_password']
     user = request.data
     to_filter = []
 
-#{"changeColumn":"title","changeValueTo":"Yellow Submarine", "artist": "The Beatles","title":"old song"}
-    if 'changeColumn' in user and 'changeValueTo' in user and 'username' in user and 'password' in user:
-        username = user['username']
-        password = user['password']
+    if 'changeColumn' in user and 'changeValueTo' in user and 'hashed_password' in user:
+        hashed_password = user['hashed_password']
         columnName = user['changeColumn']
         columnValue =user['changeValueTo']
-        query = "UPDATE users SET {}=? WHERE username=? AND password=?".format(columnName)
+        query = "UPDATE users SET {}=? WHERE password=?".format(columnName)
         to_filter.append(columnValue)
-        to_filter.append(username)
-        to_filter.append(password)
+        to_filter.append(hashed_password)
         queries._engine.execute(query,to_filter)
-#{"changeColumn":"title","changeValueTo":"Yellow Submarine","id":"2"}
+
     elif 'id' in user and 'username' in user:
         columnName = user['changeColumn']
         columnValue = password['changeValueTo']
@@ -94,14 +106,11 @@ def update_user(user):
     return user, status.HTTP_201_CREATED
 
 
-#Search for track based off given parameter
+#Search for users based off given parameter
 def filter_users(query_parameters):
     id = query_parameters.get('id')
     username = query_parameters.get('username')
-    password = query_parameters.get('password')
-    firstname = query_parameters.get('firstname')
-    #lastname = query_parameters.get('lastname')
-    #email = query_parameters.get('email')
+    hashed_password = query_parameters.get('hashed_password')
 
     query = "SELECT * FROM users WHERE"
     to_filter = []
@@ -113,9 +122,9 @@ def filter_users(query_parameters):
         query += ' username=? AND'
         to_filter.append(username)
     if password:
-        query += ' password=? AND'
-        to_filter.append(password)
-    if not (id or username or password or firstname):
+        query += ' hashed_password=? AND'
+        to_filter.append(hashed_password)
+    if not (id or username or hashed_password):
         raise exceptions.NotFound()
     query = query[:-4] + ';'
 
